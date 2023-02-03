46cff593521bd736b8b0b1dc8954ce34065132f1
kdat21 <buikhacdat1@gmail.com>
2023-02-03 11:21:42 +0700

    preview pdf file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Worker, Viewer, SpecialZoomLevel } from '@react-pdf-viewer/core'
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import { Button, Input } from 'antd';
 
 function App() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const [docsURL, setDocsURL] = useState('');
+  const [document, setDocument] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="">
+        <label htmlFor="">
+          <h5 style={{ textAlign: 'center' }}>UPLOAD PDF FILE</h5>
+          <br />
+          <Input.Group compact>
+            <Input style={{ width: 'calc(100% - 110px)' }} addonBefore="Current file URL" onChange={(e) => setDocsURL(e.target.value)} />
+            <Button type='primary' onClick={() => setDocument(docsURL)}>Update URL</Button>
+          </Input.Group>
+        </label>
+      </form>
+      <h5 style={{ textAlign: 'center' }}>VIEW PDF</h5>
+      <div>
+        {document && (
+          <Worker workerUrl='https://unpkg.com/pdfjs-dist@3.3.122/build/pdf.worker.min.js'>
+            <Viewer
+              fileUrl={document}
+              plugins={[defaultLayoutPluginInstance]}
+              defaultScale={SpecialZoomLevel.PageWidth}
+            />
+          </Worker>
+        )}
+      </div>
     </div>
   );
 }
